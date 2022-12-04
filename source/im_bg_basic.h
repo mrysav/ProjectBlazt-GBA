f1f7c8d7860e76fff85be0d79c3c86a4c6e68617
Mitchell Rysavy <mitchell.rysavy@gmail.com>
2022-12-04 11:28:03 -0500

    add 0 and 9

--- source/im_bg_basic.h
+++ source/im_bg_basic.h
@@ -6,12 +6,12 @@
 //	im_bg_basic, 256x256@8, 
 //	Transparent color : FF,00,E1
 //	+ palette 256 entries, not compressed
-//	+ 43 tiles (t|f reduced) not compressed
+//	+ 51 tiles (t|f reduced) not compressed
 //	+ regular map (in SBBs), not compressed, 16x16 
 //	Metatiled by 2x2
-//	Total size: 512 + 2752 + 128 + 512 = 3904
+//	Total size: 512 + 3264 + 144 + 512 = 4432
 //
-//	Time-stamp: 2022-12-02, 16:43:19
+//	Time-stamp: 2022-12-04, 11:23:41
 //	Exported by Cearn's GBA Image Transmogrifier, v0.9.2
 //	( http://www.coranac.com/projects/#grit )
 //
@@ -20,11 +20,11 @@
 #ifndef GRIT_IM_BG_BASIC_H
 #define GRIT_IM_BG_BASIC_H
 
-#define im_bg_basicTilesLen 2752
-extern const unsigned int im_bg_basicTiles[688];
+#define im_bg_basicTilesLen 3264
+extern const unsigned int im_bg_basicTiles[816];
 
-#define im_bg_basicMetaTilesLen 128
-extern const unsigned short im_bg_basicMetaTiles[64];
+#define im_bg_basicMetaTilesLen 144
+extern const unsigned short im_bg_basicMetaTiles[72];
 
 #define im_bg_basicMetaMapLen 512
 extern const unsigned short im_bg_basicMetaMap[256];
